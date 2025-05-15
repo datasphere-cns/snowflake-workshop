1264c386e9131e8afb8ed6ab2f1bb2ccaebb3559
Nelson Zepeda <48064346+nzepedacc@users.noreply.github.com>
2025-05-15 17:29:07 -0600

    Add files via upload

---
+++ CargaDatos/cargar_products_a_snowflake.py
@@ -0,0 +1,46 @@
+## pip install snowflake-connector-python
+
+import snowflake.connector
+import os
+
+# Configuración de conexión
+conn = snowflake.connector.connect(
+    user='usuario_developer',
+    password='Dev#2025',
+    account='kixfvtu-oic64833',
+    warehouse='WH_SMALL',
+    database='WORKSHOP',
+    schema='BRONZE_MERCADEO',
+    role='ROLE_DEVELOPER'
+)
+
+# Ruta local del archivo CSV
+local_file_path = 'C:/Snowflake/CargaDatos/dataset/products.csv'
+
+# Nombre de archivo
+file_name = os.path.basename(local_file_path)
+
+# Ejecutar comandos
+cs = conn.cursor()
+try:
+    # Comando PUT para subir archivo al STAGE interno
+    put_command = f"PUT file://{local_file_path} @STG_PRODUCTS_DEV auto_compress=false;"
+    cs.execute(put_command)
+    print("Archivo cargado al STAGE exitosamente.")
+
+    # COPY INTO desde el stage
+    copy_command = f"""
+    COPY INTO PRODUCTS_BRONZE
+    FROM @STG_PRODUCTS_DEV/{file_name}
+    FILE_FORMAT = (
+        TYPE = 'CSV',
+        FIELD_OPTIONALLY_ENCLOSED_BY = '"',
+        SKIP_HEADER = 1
+    )
+    ON_ERROR = 'CONTINUE';
+    """
+    cs.execute(copy_command)
+    print("Datos copiados a PRODUCTS_BRONZE.")
+finally:
+    cs.close()
+    conn.close()
